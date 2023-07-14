02d4f34f2a9d039cebde6d0ce1f7dbd5e4746f94
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-07-14 04:32:53 -0700

    print numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned int j;
+
+	for (j = 0; j < 10; j++)
+		putchar('0' + j);
+	putchar('\n');
+	return (0);
+}
