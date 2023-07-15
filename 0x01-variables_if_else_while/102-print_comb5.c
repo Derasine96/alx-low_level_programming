5fa9fe87c41ebf3e6801fb5f08f6a0f8128f48ac
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-07-14 17:15:03 -0700

    a program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ int main(void)
 			putchar(' ');
 			putchar(a / 10 + '0');
 			putchar(b % 10 + '0');
-		if (a == 98 && b == 99)
+		if (a == 99 && b == 98)
 		continue;
 			putchar(',');
 			putchar(' ');
