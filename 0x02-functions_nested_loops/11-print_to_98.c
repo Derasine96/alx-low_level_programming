c054f677b07ae4a4aa3ee4dc7364c8fa5b5fe0ca
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-07-18 10:01:02 -0700

    a function that prints all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,15 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n >= 98)
 	{
-		_putchar((i % 10) + '0');
-		if (i == 98)
-		break;
-		_putchar(',');
-		_putchar(' ');
+		for (i = n; i > 98; i--)
+			_putchar((i & 10) + '0');
 	}
-	putchar('\n');
+	else
+	{
+		for (i = n; i < 98; i++)
+			_putchar((i % 10) + '0');
+	_putchar('\n');
+	}
 }
