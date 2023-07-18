1752a6398f8c6729950311ecf83f71f413528636
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-07-18 09:32:01 -0700

    a function that prints all natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - a function that prints all natural numbers
+ *
+ * @n: integerto be checked
+ *
+ * Return: Aways 0 (Success)
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		_putchar(i);
+		if (i == 98)
+		break;
+	_putchar(',');
+	_putchar(' ');
+	}
+}
