c9965849fec23dea93a92544346ade65623d3ec8
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-07-19 06:42:40 -0700

    a function that prints the n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_times_table - a function that prints the n times table
+ *
+ * @n: integer value to be passed
+ *
+ * Return: Void
+ */
+void print_times_table(int n)
+{
+	int a, b, r;
+
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+	for (a = 0; a <= n; a++)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			r = a * b;
+			if (r <= 9)
+			{
+				_putchar(' ');
+				_putchar('0' + r);
+			}
+			else
+			{
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
