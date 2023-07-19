43559f6637f4e83301b9218d5be4b28bd8b758f1
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-07-19 14:39:49 -0700

    a program that computes and prints the sum of all the multiples of 3 or 5

---
+++ 0x03-debugging/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - program that computes and prints the sum of numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
