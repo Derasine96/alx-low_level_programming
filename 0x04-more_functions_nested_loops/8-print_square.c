b60a3fafb61b7b0a009c447c9e14a7061dca246c
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-07-20 08:32:34 -0700

    Function to print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - a function that prints a square
+ * @size: size to be checked
+ * Return: Void
+ */
+void print_square(int size)
+{
+	int s, q;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (s = 0; s < size; s++)
+	{
+		for (q = 0; q < size; q++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
