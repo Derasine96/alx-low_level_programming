6f89030a8432a1916c8c5bd49fb341179a80bbdc
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-07-24 06:02:01 -0700

    Made neccessary corrections

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,12 +9,12 @@ void puts_half(char *str)
 	int half;
 	int length_of_the_string = 0;
 
-	while (str[length_of_the_string] != 0)
+	while (str[length_of_the_string] != '\0')
 	{
 		length_of_the_string++;
 	}
 	half = (length_of_the_string) / 2;
-	while (str[half] != 0)
+	while (str[half] != '\0')
 	{
 		_putchar(str[half]);
 		half++;
