d7261d8acc147882ead906926d33a5c1d77732b9
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-07-27 14:46:30 -0700

    a function that encodes a string using rot13.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
- * */
+ * rot13 - a function that encodes a string using rot13.
+ * @rot: a string to be checked
+ * Return: Character
+ */
 char *rot13(char *rot)
 {
 	int a = 0;
@@ -8,7 +11,8 @@ char *rot13(char *rot)
 	char t[] = "MLKJIHGFEDCBAmlkjihgfedcbaZYXWVUTSRQPONzyxwvutsrqpon";
 	int d;
 	int length = 51;
-	while (rot[a]!= '\0')
+
+	while (rot[a] != '\0')
 	{
 		for (d = 0; s[d] != '\0'; d++)
 		{
