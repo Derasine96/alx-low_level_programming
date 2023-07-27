659dc05955a83d98609634dc9f7298e0077ddac9
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-07-27 12:18:31 -0700

    a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,6 +18,12 @@ char *cap_string(char *s)
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				s[i + 1] = s[i + 1] - 32;
 		}
+		else if (s[0] >= 'a' && s[0] <= 'z')
+		{
+			s[0] = s[0] - 32;
+		}
+		else 
+			s[i] = s[i];
 		i++;
 	}
 	return (s);
