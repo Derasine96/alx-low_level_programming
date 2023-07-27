89b117e286105ecd64595501dc9405d86e43c7d4
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-07-27 12:00:47 -0700

    a function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * leet - a function that encodes a string
+ * @y: a string to be checked
+ * Return: Character
+ */
+char *leet(char *y)
+{
+	int b;
+	char r[] = "AaEeOoTtLl";
+
+	while (r[b] != '\0')
+	{
+		for (b = 0; y[b] != '\0'; b++)
+		{
+			r[0] = 4;
+		}
+		b++;
+	}
+	return (y);
+}
