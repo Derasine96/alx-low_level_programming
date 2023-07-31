7c5f105a8137f3a016b9d34912f56fcdc488a5e8
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-07-31 12:02:02 +0100

    made a betty correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,7 +10,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 	unsigned char value = (unsigned char)b;
-	unsigned char *mem = (unsigned char*)s; 
+	unsigned char *mem = (unsigned char *)s;
 
 	for (i = 0; i < n; i++)
 	{
