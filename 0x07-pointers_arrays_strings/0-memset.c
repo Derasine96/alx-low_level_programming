4bef76d8572966a085427724854aefd4e8a3a4d0
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-07-31 11:59:13 +0100

    A function that memset and memcpy

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - a function that fills memory with a constant byte.
+ * @s: pointer to the mem. block
+ * @b: Value you want to set in mem. block
+ * @n: number of bytes you want to set
+ * Return: Character
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	unsigned char value = (unsigned char)b;
+	unsigned char *mem = (unsigned char*)s; 
+
+	for (i = 0; i < n; i++)
+	{
+		*mem = value;
+		mem++;
+	}
+	return (s);
+}
