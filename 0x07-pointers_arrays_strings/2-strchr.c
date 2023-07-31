3977eb381897a2acb653be10308f357486102585
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-07-31 12:31:33 +0100

    A function that locates a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - a function that locates a character in a string.
+ * @s: charcter to be checked
+ * @c: character to be checked
+ * Return: Pointer to character
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
