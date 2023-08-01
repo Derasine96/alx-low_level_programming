cc897be4d660f30f6e15f3112e83c33616d9d993
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-08-01 02:19:16 +0100

    Made corrections

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,27 +1,22 @@
 #include "main.h"
 /**
- *  * _strchr - a function that locates a character in a string.
- *   * @s: charcter to be checked
- *    * @c: character to be checked
- *     * Return: Pointer to character
- *      */
+ * * _strchr - a function that locates a character in a string.
+ * @s: charcter to be checked
+ * @c: character to be checked
+ * Return: Pointer to character
+ */
 char *_strchr(char *s, char c)
 {
-<<<<<<< HEAD
 	unsigned int i, count = 0;
 
 	while (s[count] != '\0')
 		count++;
 	for (i = 0; i <= count; i++)
-=======
-
-	while (*s != '\0')
->>>>>>> 488c1050778fe151751824e6814edab0a21c8ed8
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
 			return (s + i);
 		}
 	}
-	return (NULL);
+	return ('\0');
 }
