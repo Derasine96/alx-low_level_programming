d938e2642fa31d9bd25e6b863126939b0303b996
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-08-10 04:20:37 -0700

    a function that allocates memory using malloc.

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,15 +2,16 @@
 #define MAIN_H
 
 #include <stdio.h>
-
-int _putchar(char c);
-void *malloc_checked(unsigned int b);
+#include <stdlib.h>
+/**int _putchar(char c);
+void *malloc_checked(unsigned int b);*/
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
+/**void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void simple_print_buffer(char *buffer, unsigned int size)
 void simple_print_buffer(int *buffer, unsigned int size);
+*/
 
 #endif /* MAIN_H */
 
