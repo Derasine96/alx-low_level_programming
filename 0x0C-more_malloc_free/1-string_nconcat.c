d2930bc6cdc6478a614a13327b759017fb5dc5fc
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-08-10 04:55:22 -0700

    Corrections

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * string_nconcat - function that concatenates two strings.
  * @s1: destination string
@@ -8,30 +9,31 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, len1, len2;
+	unsigned int i, j, num, len1, len2;
 	char *str;
 
+	num = n;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 	while (*s1 != '\0')
 	{
-		len1 += 1;
+		len1++;
 	}
 	while (*s2 != '\0')
 	{
-		len2 += 1;
+		len2++;
 	}
-	if (n > len2)
-		n = len2;
-	str = (char *)malloc((len1 + n + 1) * sizeof(char));
+	if (num >= len2)
+		num = len2;
+	str = (char *)malloc((len1 + num + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
-	for (i = 0; i < n; i++)
-		str[len1 + i] = s2[i];
-	str[len1 + n] = '\0';
+	for (j = 0; j < num; j++)
+		str[i + j] = s2[j];
+	str[i + j] = '\0';
 	return (str);
 }
