044936819fdd88c149a90010f59401e0c166e214
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-08-16 19:40:16 -0700

    a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - a program that performs simple operations.
+ * @argc: no of arguments
+ * @argv: arguments passed
+ * Return: Integer
+ */
+int main(int argc, char *argv[])
+{
+	int (*func)(int, int);
+	int num1, num2, result;
+	char s;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	func = get_op_func(argv[2]);
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	s = *argv[2];
+	if ((s == '/' || s == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = func(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add - function to sum two integers
+ * @a: first integers
+ * @b: second integer
+ * Return: Sum of integers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function to subtract two integers
+ * @a: first integers
+ * @b: second integer
+ * Return: Sub of integers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function to multiply two integers
+ * @a: first integers
+ * @b: second integer
+ * Return: Mult. of integers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function to divide two integers
+ * @a: first integers
+ * @b: second integer
+ * Return: Division of integers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function to find remainder two integers
+ * @a: first integers
+ * @b: second integer
+ * Return: Modulus of integers
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
