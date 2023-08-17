1f8ba3f3ec8949599f778fa2627a8d23606ed121
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-08-17 14:32:30 -0700

    A function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,58 +1,48 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
-/**
- * print_format - a function that prints format
- * @c: character to be checked
- * @args: differnt arguments
- */
-void print_format(char c, va_list *args)
-{
-	char *content;
 
-	switch (c)
-	{
-		case 'c':
-			printf("%c", va_arg(*args, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(*args, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(*args, double));
-			break;
-		case 's':
-			content = va_arg(*args, char *);
-			if (content == NULL)
-				printf("(nil)");
-			printf("%s", content);
-			break;
-	}
-}
 /**
- * print_all - a function that prints anything
- * @format: is a list of types of args. passed to the func
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0, num_args = strlen(format);
+	int i = 0;
+	char *str, *sep = "";
+
 	va_list args;
 
 	va_start(args, format);
-	while (i < num_args)
+
+	if (format)
 	{
-		if (format[i] == '\0')
+		while (format[i])
 		{
-			printf("\n");
-			return;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(args, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(args, double));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
 		}
-		print_format(format[i], &args);
-		i++;
-		if ((i > 0) && (format[i] == 'c' || format[i] == 'i'
-					|| format[i] == 'f' || format[i] == 's'))
-			printf(", ");
 	}
+
 	printf("\n");
 	va_end(args);
 }
