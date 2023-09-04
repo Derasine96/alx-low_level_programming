5a9d25928bc3ab2f3ec300d445857f923b3fbe47
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-09-04 07:50:48 -0700

    A function to create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,22 +14,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
-	if (fd == -1)
-		return (-1);
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
-		len++;
+			len++;
 	}
-	if (text_content == NULL)
-	{
-		bytesWrite = write(fd, text_content, len);
-		if (bytesWrite == -1)
-		{
-			return (-1);
-		}
-	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	bytesWrite = write(fd, text_content, len);
+
+	if (fd == -1 || bytesWrite == -1)
+		return (-1);
 	close(fd);
 	return (1);
 }
