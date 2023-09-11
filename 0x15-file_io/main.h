bbb08e592988d60eab6369fd1bcb16de098376e3
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-09-11 06:17:37 -0700

    displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,10 +2,13 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <stdarg.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
