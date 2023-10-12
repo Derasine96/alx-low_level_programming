0d987ab7e872de6c764c012666800e96a384ab35
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-10-12 07:26:31 -0700

    insert node at given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,40 +10,40 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    unsigned int i = 0;
-    dlistint_t *new, *temp, *prev = NULL;
+	unsigned int i = 0;
+	dlistint_t *new, *temp, *prev = NULL;
 
-    if (h == NULL)
-        return (NULL);
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = n;
-    if (idx == 0) {
-        new->prev = NULL;
-        new->next = *h;
-        if (*h)
-            (*h)->prev = new;
-        *h = new;
-        return (new);
-    }
-    temp = *h;
-    while (temp && i < idx)
-    {
-        prev = temp;
-        temp = temp->next;
-        i++;
-    }
-    if (i == idx)
-    {
-        new->prev = prev;
-        new->next = temp;
-        if (temp)
-            temp->prev = new;
-        prev->next = new;
-        return (new);
-    }
-    free(new);
-    return (NULL);
+	if (h == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 0) {
+		new->prev = NULL;
+		new->next = *h;
+		if (*h)
+			(*h)->prev = new;
+		*h = new;
+		return (new);
+	}
+	temp = *h;
+	while (temp && i < idx)
+	{
+		prev = temp;
+		temp = temp->next;
+		i++;
+	}
+	if (i == idx)
+	{
+		new->prev = prev;
+		new->next = temp;
+		if (temp)
+			temp->prev = new;
+		prev->next = new;
+		return (new);
+	}
+	free(new);
+	return (NULL);
 }
 
