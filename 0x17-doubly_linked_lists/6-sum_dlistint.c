07e4826740a41c0a567c33c662d520e019eeed7d
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-10-12 05:35:22 -0700

    sum of elements of a doubly linked list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_dlistint - function that returns the sum of data
+ * @head: head of list
+ * Return: Sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	temp = head;
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
