4dad5fcc65a25a024b9ebf32fed814fbd9fff592
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-02 15:03:13 -0700

    Fix Task0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,12 +7,12 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i = 0;
-	hash_table_t *table = malloc(sizeof(hash_table_t));
+	hash_table_t *table = malloc(sizeof(*table));
 
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t *) * size);
+	table->array = malloc(sizeof(*table->array) * size);
 	if (table->array == NULL)
 	{
 		free(table);
