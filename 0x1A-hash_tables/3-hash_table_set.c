14f35cae6f32d557296d135b9009808206df0c9d
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-03 09:02:38 -0700

    Fix task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,29 +8,34 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *node;
-
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-	index = hash_djb2((const unsigned char *)key) % ht->size;
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
-		return (0);
-	node->key = strdup(key);
-	if (node->key == NULL)
-	{
-		free(node);
-		return (0);
-	}
-	node->value = strdup(value);
-	if (node->value == NULL)
-	{
-		free(node->key);
-		free(node);
-		return (0);
-	}
-	node->next = ht->array[index];
-	ht->array[index] = node;
-	return (1);
+    unsigned long int index;
+    hash_node_t *node. *current_head;
+    
+    if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+        return (0);
+    index = hash_djb2((const unsigned char *)key) % ht->size;
+    node = malloc(sizeof(hash_node_t));
+    if (node == NULL) {
+        return (0);
+    }
+    node->key = strdup(key);
+    if (node->key == NULL)
+    {
+        free(node);
+        return (0);
+    }
+    node->value = strdup(value);
+    if (node->value == NULL)
+    {
+        free(node->key);
+        free(node);
+        return (0);
+    }
+    if (ht->array[index] != NULL)
+    {
+        current_head = ht->array[index];
+        node->next = current_head; 
+    }
+    ht->array[index] = node;
+    return (1);
 }
