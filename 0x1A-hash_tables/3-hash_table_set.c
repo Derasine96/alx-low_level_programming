4841d1ceb7becbffb03ea78a6e43b2bc803b3459
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-03 09:21:21 -0700

    Fix task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * hash_table_set - Function that adds an element to the hash table.
+ * hash_table_set - Function that adds or updates an element in the hash table.
  * @ht: Pointer to the hash table
  * @key: The key for which to compute the index.
  * @value: Value stored in the key
@@ -9,15 +9,28 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
     unsigned long int index;
-    hash_node_t *node, *current_head;
-    
+    hash_node_t *node;
+    hash_node_t *current_node;
+
     if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
         return (0);
     index = hash_djb2((const unsigned char *)key) % ht->size;
-    node = malloc(sizeof(hash_node_t));
-    if (node == NULL) {
-        return (0);
+    current_node = ht->array[index];
+    while (current_node != NULL)
+    {
+        if (strcmp(current_node->key, key) == 0)
+        {
+            free(current_node->value);
+            current_node->value = strdup(value);
+            if (current_node->value == NULL)
+                return (0);
+            return (1);
+        }
+        current_node = current_node->next;
     }
+    node = malloc(sizeof(hash_node_t));
+    if (node == NULL)
+        return (0);
     node->key = strdup(key);
     if (node->key == NULL)
     {
@@ -31,11 +44,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         free(node);
         return (0);
     }
-    if (ht->array[index] != NULL)
-    {
-        current_head = ht->array[index];
-        node->next = current_head; 
-    }
+    node->next = ht->array[index];
     ht->array[index] = node;
     return (1);
 }
