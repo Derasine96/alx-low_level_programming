55f3628ca7679996d32e9c02432e72758c8166ab
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-03 09:22:13 -0700

    Fix task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,43 +8,43 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *node;
-    hash_node_t *current_node;
+	unsigned long int index;
+	hash_node_t *node;
+	hash_node_t *current_node;
 
-    if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-        return (0);
-    index = hash_djb2((const unsigned char *)key) % ht->size;
-    current_node = ht->array[index];
-    while (current_node != NULL)
-    {
-        if (strcmp(current_node->key, key) == 0)
-        {
-            free(current_node->value);
-            current_node->value = strdup(value);
-            if (current_node->value == NULL)
-                return (0);
-            return (1);
-        }
-        current_node = current_node->next;
-    }
-    node = malloc(sizeof(hash_node_t));
-    if (node == NULL)
-        return (0);
-    node->key = strdup(key);
-    if (node->key == NULL)
-    {
-        free(node);
-        return (0);
-    }
-    node->value = strdup(value);
-    if (node->value == NULL)
-    {
-        free(node->key);
-        free(node);
-        return (0);
-    }
-    node->next = ht->array[index];
-    ht->array[index] = node;
-    return (1);
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+	current_node = ht->array[index];
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value);
+			current_node->value = strdup(value);
+			if (current_node->value == NULL)
+				return (0);
+			return (1);
+		}
+		current_node = current_node->next;
+	}
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
 }
