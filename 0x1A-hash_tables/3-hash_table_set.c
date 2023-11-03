941ef2cb4e4d06f07d5614a61342198d639d376c
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-03 08:30:55 -0700

    Task3 fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,29 +9,21 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node;
+	unsigned long index;
+	hash_node_t *node;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (key == NULL || *key == '\0')
 		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-	return (1);
+	index = hash_function(key);
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return 0;
+	node->key = strdup(key);
+	node->value = strdup(value);
+	if (ht->array[index] != NULL)
+		node->next = ht->array[index];
+	else
+		node->next = NULL;
+	ht->array[index] = node;
+	return 1;
 }
