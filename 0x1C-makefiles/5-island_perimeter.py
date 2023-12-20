7472c727ede66fc3e63ea1807c05c68da20099e7
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-12-20 14:42:04 -0800

    Finding the perimeter of a grid. Module and Documentation

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Perimeter of an island"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island
+
+    Args:
+        grid(int): list of list of integers
+    """
