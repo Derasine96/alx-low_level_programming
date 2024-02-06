2d6c2ff65bb41d16b555eea2996fb32b6a4437bb
Obikwelu Chidera <chidexobikwelu@gmail.com>
2024-02-06 05:58:30 -0800

    Added check for size

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,11 +8,11 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i;
 
 	if (array == NULL || size == 0)
 		return (-1);
-	for (i = 0; i < int(size); i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked at array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
